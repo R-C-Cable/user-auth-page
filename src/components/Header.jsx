c645b0fe7a0ec2d95b85aff9f9d7b664e97f458a
Robert <r.c.cable@tutamail.com>
2021-06-10 10:12:11 -0600

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {Navbar, Nav} from "react-bootstrap";
 function Header() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">User Sign-In Test Page</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
--- src/components/LogInCard.jsx
+++ src/components/LogInCard.jsx
@@ -1,36 +1,54 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Card, Nav, Button, Container} from "react-bootstrap";
-import "./components.css"
+import {
+  Card,
+  Nav,
+  Button,
+  Container,
+  Form,
+} from "react-bootstrap";
+import "./components.css";
 
-
-
-
+function callLogIn() {}
 
 function LogInCard() {
   return (
     <Container className="pt-3">
-    <Card>
-      <Card.Header>
-        <Nav variant="tabs" defaultActiveKey="#first">
-          <Nav.Item>
-            <Nav.Link href="#first">Log in</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#link">Sign Up</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Card.Header>
+      <Card>
+        <Card.Header>
+          <Nav variant="tabs" defaultActiveKey="#first">
+            <Nav.Item>
+              <Nav.Link onSelect="callLogIn" href="#first">
+                Log in
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#link">Sign Up</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
 
+        <Card.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Sign In</Button>
-      </Card.Body>
-    </Card>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox"></Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
